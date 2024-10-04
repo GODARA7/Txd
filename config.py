50356cd25b7fb1afabc0a8d86aa47670211a49e1
pkbot72 <101813111+pkbot72@users.noreply.github.com>
2024-10-04 09:06:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", "20407127"))  
+API_ID = int(os.environ.get("API_ID", "9741228"))  
 
-API_HASH = os.environ.get("API_HASH", "877e7db614293fb2d7c5e4caa79896f7")
+API_HASH = os.environ.get("API_HASH", "6f339e8fa827a5147fa2ff03898d610c")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7447109300:AAG2RVJClcMUIcFPagYMO3z5e3OTqTOyhJw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6893022010:AAFFY4G8ouuBCi0yWxLWCQ31lbRZRw7AbOw")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 7535195022))
+OWNER = int(os.environ.get("OWNER", 809150135))
 
-LOG = -1002116155974
+LOG = -1001973729974
 
 # UPDATE_GRP = , # bot sat group
 
@@ -18,7 +18,7 @@ LOG = -1002116155974
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "7535195022").split()):
+    for x in (os.environ.get("ADMINS", "809150135").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
